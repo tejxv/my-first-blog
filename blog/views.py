1ed0cc1b0d2c2f217d19d3411f9f4bc2e61b974b
tejas <upadhyaytejas100@gmail.com>
2020-01-08 16:46:09 +0530

    edited html, made it pretty

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
+
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
