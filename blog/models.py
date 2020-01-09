c23fa4f52959538436c0835ab930582c764c3df5
tejas <upadhyaytejas100@gmail.com>
2020-01-10 03:38:30 +0530

    3rd attempt

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=20)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
