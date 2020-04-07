12d4954a15378c6112befdeda065c936ae8a840f
tejas <upadhyaytejas100@gmail.com>
2020-04-07 18:10:50 +0530

    fix migration

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w=mvijg7f+)$ck*c1&7xjyrvbctsmy&6!@#=e7#idpn!l4(x5c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
